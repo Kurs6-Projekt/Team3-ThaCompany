7b02a6d1c15eb7f1f8f24307da9ed82522a11014
Questionable Developer <questionable.developer@example.com>
2026-09-20 17:19:15 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dev-secret-key'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7000)
